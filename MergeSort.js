8ca3c6f06e1442b10e191cc03c718ad63496cd70
ShallyLai <79987875+ShallyLai@users.noreply.github.com>
2022-05-22 23:03:52 +0800

    add_filter_2

---
+++ MergeSort.js
@@ -0,0 +1,33 @@
+// Merge sort for every university 
+function MergeSort(array){
+  if (array.length <= 1){
+    return array;
+  };
+  const middle = Math.floor(array.length / 2);
+  //console.log("Hi MergeSort");
+  return Merge(MergeSort(array.slice(0, middle)), MergeSort(array.slice(middle)));
+}
+  
+function Merge(left, right){
+  let result = [];
+  let leftIndex = 0;
+  let rightIndex = 0;
+  while(leftIndex < left.length && rightIndex < right.length){
+    // console.log("Hi Merge");
+    let leftValue = Number(left[leftIndex].ResearchOutput) + left[leftIndex].SFRatio + left[leftIndex].InterStu + left[leftIndex].FC;
+    let rightValue = Number(right[rightIndex].ResearchOutput) + right[rightIndex].SFRatio + right[rightIndex].InterStu + right[rightIndex].FC;
+    if(leftValue > rightValue){
+      result.push(left[leftIndex]);
+      leftIndex++;
+      //console.log("Hi Left");
+      //console.log(leftValue, rightValue);
+    } 
+    else{
+      result.push(right[rightIndex]);
+      rightIndex++;
+      //console.log("Hi right");
+      //console.log(leftValue, rightValue);
+    }
+  }
+  return result.concat(left.slice(leftIndex)).concat(right.slice(rightIndex));
+}
--- main.js
+++ main.js
@@ -52,40 +52,6 @@ function RSOutput(score){
     return ("Low");
 }
 
-// Merge sort for every university 
-function MergeSort(array){
-  if (array.length <= 1){
-    return array;
-  };
-  const middle = Math.floor(array.length / 2);
-  //console.log("Hi MergeSort");
-  return Merge(MergeSort(array.slice(0, middle)), MergeSort(array.slice(middle)));
-}
-  
-function Merge(left, right){
-  let result = [];
-  let leftIndex = 0;
-  let rightIndex = 0;
-  while(leftIndex < left.length && rightIndex < right.length){
-    // console.log("Hi Merge");
-    let leftValue = Number(left[leftIndex].ResearchOutput) + left[leftIndex].SFRatio + left[leftIndex].InterStu + left[leftIndex].FC;
-    let rightValue = Number(right[rightIndex].ResearchOutput) + right[rightIndex].SFRatio + right[rightIndex].InterStu + right[rightIndex].FC;
-    if(leftValue > rightValue){
-      result.push(left[leftIndex]);
-      leftIndex++;
-      //console.log("Hi Left");
-      //console.log(leftValue, rightValue);
-    } 
-    else{
-      result.push(right[rightIndex]);
-      rightIndex++;
-      //console.log("Hi right");
-      //console.log(leftValue, rightValue);
-    }
-  }
-  return result.concat(left.slice(leftIndex)).concat(right.slice(rightIndex));
-}
-
 // when the input range changes update the weight 
 d3.select("#nWeight_ResearchO").on("input", function() {
   update(+this.value, "Research",clonedThisYear);
